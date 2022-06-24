e1a6533c75a86c3fc81d655578de5c5509caaf01
dev-com2020 <kwp@tlen.pl>
2022-06-24 12:23:02 +0200

    pt 26.06 g.12:22

---
+++ trzeci_test.py
@@ -0,0 +1,9 @@
+def myapp():
+    print("Aplikacja została uruchomiona")
+
+
+def test_capsys(capsys):
+    myapp()
+    out, err = capsys.readouterr()
+    assert out == "Aplikacja została uruchomiona\n"
+
